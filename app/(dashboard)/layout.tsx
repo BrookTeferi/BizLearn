30ecc70be8266ccf2c7b553eeff0b753b2710dac
BrookTeferi <brookteferi1q2w@gmail.com>
2024-02-25 16:54:06 +0300

    sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+const DashBoardLayout = ({
+children 
+}:{
+ children: React.ReactNode;
+}) => {
+    return ( 
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50"></div>
+
+            {children}
+        </div>
+     );
+}
+ 
+export default DashBoardLayout
---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+const TestPage = () => {
+    return ( 
+<div>
+    This page should be unprotected
+</div>
+
+     );
+}
+ 
+export default TestPage;
