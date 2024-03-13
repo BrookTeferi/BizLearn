6361a27aacc60dc7e15d4a83f1c9b606a0a9910f
BrookTeferi <brookteferi1q2w@gmail.com>
2024-03-13 11:32:43 +0300

    Add - Dashbord - Sidebar item

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 interface SidebarItemProps{
     icon:LucideIcon;
     label: string;
@@ -16,11 +17,39 @@ export const SidebarItem = ({
 }:SidebarItemProps) => {
 
     const pathname= usePathname();
+    const router= useRouter();
+    const isActive = (pathname === "/" && href === "/") || pathname===href || pathname?.startsWith('${href}/');
 
-    
 
-return (
-    <div>
-        sidebarItem!
-        </div>
-)};
+
+    const onClick = () =>{
+        router.push(href);  
+    }
+
+    return(
+        <button
+        onClick={onClick}
+        type="button"
+        className={cn(
+            "flex items-center gap-x-2 text-slate-500 text-sm front-[500] pl-6 transistion-all hover:text-slate-600 hover:bg-slate-300/20 ",
+            isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+        )}
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon
+                size={22}
+                className={cn(
+                    "text-slate-500", isActive && "text-sky-700"
+                )}
+                />
+                {label}
+            </div>
+
+            <div
+            className={cn(
+                "ml-auto opacity-0 border-2 border-sky-700 h-full translation-all", isActive && "opacity-100"
+            )}
+            />
+        </button>
+    )
+}
