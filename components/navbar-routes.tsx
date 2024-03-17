89c3afa480d3449ffc752e7cfbc23a37ef24a80a
brookteferi1q2w@gmail.com <brookteferi1q2w@gmail.com>
2024-03-17 14:11:17 +0300

    fix course page.tsx

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -19,11 +19,12 @@ export const NavbarRoutes = () => {
    
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isCoursePage ? (
-        
+            <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
               Exit
             </Button>
+            </Link>
          
         ) :(
           <Link href="/teacher/courses">
